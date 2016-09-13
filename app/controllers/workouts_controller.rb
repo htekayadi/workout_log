64e72549a1fafdc48f960da826adc7822315eb5a
Hendrik Tekayadi <hendrik.tekayadi@jurnal.id>
2016-09-13 23:17:11 +0700

    Add edit & destroy ability

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,8 +1,8 @@
 class WorkoutsController < ApplicationController
-  before_action :find_workout, only: [:show, :edit, :udpate, :destroy]
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @workouts = Workout.all.order("created_at DESC")
   end
 
   def show
@@ -27,11 +27,16 @@ class WorkoutsController < ApplicationController
   end
 
   def update
-
+    if @workout.update(workout_params)
+      redirect_to @workout
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @workout.destroy
+    redirect_to root_path
   end
 
   private
